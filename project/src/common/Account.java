5397fac579ef4c51677020a7d0b70d2a23dbaed6
DrKwint <pquint@CSELAB4-04.cs.unl.edu>
2013-07-05 13:00:22 -0500

    Modified Transaction, Created Account and Currency

---
+++ project/src/common/Account.java
@@ -0,0 +1,10 @@
+package common;
+
+import java.util.Map;
+
+public class Account 
+{
+	private String username;		// username associated with user account
+	private int accountID;			// ID associated with user account
+	private Map<Currency, Double> currencyHoldings;	// list of all currency balances
+}
