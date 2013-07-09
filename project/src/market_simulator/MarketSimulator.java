353db90b50f34fd9d1445140980598dbfbc1568e
Andrew Koerner <andrew@k0ner.com>
2013-07-09 10:47:43 -0500

    thug life

---
+++ project/src/market_simulator/MarketSimulator.java
@@ -0,0 +1,31 @@
+package market_simulator;
+
+import javax.swing.JTextField;
+
+import common.Account;
+
+public class MarketSimulator {
+	
+	private Account account;
+	
+	public MarketSimulator(){}
+
+	public Account getAccount() {
+		return account;
+	}
+	
+	public Account getAccount(String id) {
+		return account;
+	}
+
+	public String getCurrentExchangRateUSDToEuro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getCurrentExchangRateEUROToUSD() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
