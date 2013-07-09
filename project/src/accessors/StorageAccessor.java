77d2358a4393d1475e76ba3b026c37f22c829bc0
DrKwint <DrKwint@gmail.com>
2013-07-09 16:04:30 -0500

    I am so sorry

--- project/src/accessors/StorageAccessor.java
+++ project/src/accessors/StorageAccessor.java
@@ -1,15 +1,11 @@
 package accessors;
 
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-import common.Account;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
 import common.Transaction;
 
 class StorageAccessor
@@ -17,61 +13,26 @@ class StorageAccessor
 	// Write transaction to disk
 	public void StoreTransaction(Transaction t)
 	{
+		ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "Transactions.dat");
 		try {
-		File tFile = new File("Transactions.dat");
-		if (!tFile.exists()) tFile.createNewFile();
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(tFile)));
-		out.append(t.toString()+"\n");
-		out.flush();
-		out.close();
-		} catch(IOException e) {
-			e.printStackTrace();
+			db.store(t);
+		} finally {
+			db.close();
 		}
 	}
 	
 	// Retrieve all transactions associated with a given account ID from disk
 	// Returns null if no transactions exist
-	public ArrayList<Transaction> RetrieveAccountTransactions(int AccountID)
+	protected ArrayList<Transaction> RetrieveAccountTransactions(int AccountID)
 	{
-		File tFile = new File("Transactions.dat");
-		if (!tFile.exists()) return null;
-		// Unimplemented
-		return null;
-	}
-	
-	public void StoreAccount(Account acct)
-	{
+		ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "Transactions.dat");
 		try {
-		// Make sure file works
-		File acctFile = new File(acct.getUserName()+".dat");
-		if (!acctFile.exists()) acctFile.createNewFile();
-		// Make FileWriter and write to it
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(acctFile)));
-		out.append(new Integer(acct.getId()).toString());
-		// Flush and close FileWriter
-		out.flush();
-		out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+			ObjectSet<Transaction> objs = db.query(Transaction.class);
+			ArrayList<Transaction> al = new ArrayList<Transaction>();
+			al.addAll(objs);
+			return al;
+		} finally {
+			db.close();
 		}
-	}
-	
-	// Returns null if account doesn't exist
-	public Account RetrieveAccount(String Username)
-	{
-		try {
-			File acctFile = new File(Username+".dat");
-			if (!acctFile.exists()) return null;
-			
-			 BufferedReader in = new BufferedReader(new FileReader(Username));
-			 Account ret = new Account(Username, in.readLine());
-			 
-			 in.close();
-			 
-			 return ret;
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
