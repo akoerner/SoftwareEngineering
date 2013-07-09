89b741eaf1a1319cfef715eb7a623f72227c5924
Joseph Andrew Bizal <jbizal@cse.unl.edu>
2013-07-09 13:48:00 -0500

    added methods getCurrentExchangeRateUSDtoEUR and getCurrentExchangeRateEURtoUSD to MarketAccessor

--- project/src/accessors/MarketAccessor.java
+++ project/src/accessors/MarketAccessor.java
@@ -21,14 +21,25 @@ public class MarketAccessor {
         	return rhs;
         }
     }
-	public static double getExchange() throws Exception {
+	
+	//Gets the current exchange rate of USD to EUR
+	public static double getCurrentExchangeRateUSDtoEUR() throws Exception{
+		return getCurrentExchangeRate("USD","EUR");
+	}
+	
+	//Gets the current exchange rate of EUR to USD
+	public static double getCurrentExchangeRateEURtoUSD() throws Exception{
+		return getCurrentExchangeRate("EUR","USD");
+	}
+	
+	private static double getCurrentExchangeRate(String currency1, String currency2) throws Exception {
 		
 		//strings to format url
 		String google = "http://www.google.com/ig/calculator?hl=en&q=";
-	    String baseCurrency = "USD";
-	    String termCurrency = "EUR";
-	    String charset = "UTF-8";
-
+	    String baseCurrency = currency1;
+	    String termCurrency = currency2;
+		String charset = "UTF-8";
+		
 	    URL url = new URL(google + baseCurrency + "%3D%3F" + termCurrency);
 
 	    Reader reader = new InputStreamReader(url.openStream(), charset);
