92b54a6bd88e20a2a0bbc07fa8f8f835c76be6b8
Aidan Fleischer <afleischer1337@gmail.com>
2013-07-09 11:43:59 -0500

    I added my Account_Manager class

---
+++ project/src/Account_Manager/Account_Manager.java
@@ -0,0 +1,14 @@
+
+public class Account_Manager {
+	
+	//retrieve all transactions associated with account 
+		//array list of transactions as a collection
+	 
+		//for each transaction, print the transactions as stored in the StorageAccessor account
+	for (i = 0; i < noOfTrans; i++){
+		//t is acquired from the Storage Accessor class
+		System.out.println(t);
+	}
+	
+	
+}
